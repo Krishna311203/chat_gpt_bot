4041297c5113039e9bd51d043dbdf315fe3137a0
Krishna311203 <krishnashrivastava84@gmail.com>
2023-02-25 17:26:37 +0530

    UI completed ... chat gpt task remaining

--- lib/Components/colors.dart
+++ lib/Components/colors.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const bgColor = Color(0xff00A67e);
 
 const textColor = Color(0xffFEFDFC);
+
+const chatBgColor = Color(0xff343541);
--- lib/speech_screen.dart
+++ lib/speech_screen.dart
@@ -1,7 +1,10 @@
-import 'package:chat_gpt_bot/colors.dart';
+// ignore_for_file: prefer_const_constructors
+
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:chat_gpt_bot/Components/colors.dart';
+import 'package:chat_gpt_bot/Models/chat_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class SpeechScreen extends StatefulWidget {
   const SpeechScreen({Key? key}) : super(key: key);
@@ -11,20 +14,142 @@ class SpeechScreen extends StatefulWidget {
 }
 
 class _SpeechScreenState extends State<SpeechScreen> {
+  var text = "Hold the Button and start speaking";
+  SpeechToText speechToText = SpeechToText();
+  var isListening = false;
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
-        
         centerTitle: true,
         backgroundColor: bgColor,
         elevation: 0,
         title: Text(
           "Voice GPT",
-          style: TextStyle(fontWeight: FontWeight.bold),
+          style: const TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
-      body: Container(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+        endRadius: 75,
+        animate: isListening,
+        duration: Duration(seconds: 2),
+        glowColor: bgColor,
+        repeatPauseDuration: Duration(milliseconds: 100),
+        showTwoGlows: true,
+        child: GestureDetector(
+          onTapDown: (details) async {
+            if (!isListening) {
+              var available = await speechToText.initialize();
+              if (available) {
+                setState(() {
+                  isListening = true;
+                  speechToText.listen(
+                    onResult: (result) {
+                      setState(() {
+                        text = result.recognizedWords;
+                      });
+                    },
+                  );
+                });
+              }
+            }
+          },
+          onTapUp: (details) {
+            setState(() {
+              isListening = false;
+            });
+            speechToText.stop();
+          },
+          child: CircleAvatar(
+            backgroundColor: bgColor,
+            radius: 35,
+            child: isListening ? Icon(Icons.mic) : Icon(Icons.mic_none),
+          ),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        margin: EdgeInsets.only(bottom: 5),
+        child: Column(
+          children: [
+            Text(
+              text,
+              style: TextStyle(
+                fontSize: 20,
+                color: isListening ? Colors.black : Colors.black54,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Expanded(
+              child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                  decoration: BoxDecoration(
+                    color: chatBgColor,
+                    borderRadius: BorderRadius.circular(17),
+                  ),
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: 4,
+                    itemBuilder: (BuildContext context, int index) {
+                      return chatBubble(
+                          chatText: "i love myself",
+                          type: ChatMessageType.user);
+                    },
+                  )),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Text(
+              "Developed by Krishna",
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.black54,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget chatBubble({required chatText, required ChatMessageType type}) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CircleAvatar(
+          backgroundColor: bgColor,
+          child: Icon(Icons.person, color: Colors.white),
+        ),
+        SizedBox(width: 12),
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(12),
+            margin: EdgeInsets.only(bottom: 8),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(17),
+                    bottomRight: Radius.circular(17),
+                    topRight: Radius.circular(17))),
+            child: Text(
+              chatText,
+              style: TextStyle(
+                color: chatBgColor,
+                fontSize: 15,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
